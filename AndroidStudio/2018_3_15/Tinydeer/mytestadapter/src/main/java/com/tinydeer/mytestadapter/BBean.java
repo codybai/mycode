283e9f2f631c2e6788776cc1ad605f234aa19d49
codybai <codybai@163.com>
2018-06-11 01:32:52 +0800

    校园APP

--- AndroidStudio/2018_3_15/Tinydeer/mytestadapter/src/main/java/com/tinydeer/mytestadapter/BBean.java
+++
@@ -1,8 +0,0 @@
-package com.tinydeer.mytestadapter;
-
-/**
- * Created by baicol on 2018-03-10.
- */
-
-public class BBean {
-}
--- AndroidStudio/2018_3_15/Tinydeer/mytestadapter/src/main/res/SpinnerAdapter.java
+++
@@ -1,9 +0,0 @@
-import android.widget.BaseAdapter;
-
-/**
- * Created by baicol on 2018-03-10.
- */
-
-public class SpinnerAdapter extends BaseAdapter {
-
-}
